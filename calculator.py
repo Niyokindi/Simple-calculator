4c998c56301a0e53a11954740dae8dc0809f8afe
Kenji Niyokindi <kniyokindi@gmail.com>
2022-09-15 18:36:07 -0700

    substraction method added

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
 def add(a, b):
     sum = a+b  # Adding two numbers and assigning the result to a variable "sum"
     return sum  # Returning the sum of the two numbers
+
+
+def subtract(a, b):
+    difference = a - b
+    return difference
--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,14 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.add(1, -1), 0)
         self.assertEqual(calculator.add(5, -9), -4)
         self.assertEqual(calculator.add(2.5, 1.3), 3.8)
-        self.assertEqual(calculator.add(-2.5, 1.3), -3.8)
+        self.assertEqual(calculator.add(-2.5, 1.3), -1.2)
+
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(5, 9), -4)
+        self.assertEqual(calculator.subtract(1, -1), 2)
+        self.assertEqual(calculator.subtract(5, -9), 14)
+        self.assertEqual(calculator.subtract(2.5, 1.3), 1.2)
+        self.assertEqual(calculator.subtract(-2.5, 1.3), -3.8)
 
 
 if __name__ == '__main__':
