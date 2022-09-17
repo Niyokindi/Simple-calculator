df21e0670a8294b2cb3e5f31f9b402e97dfcd8da
Kenji Niyokindi <kniyokindi@gmail.com>
2022-09-16 19:04:54 -0700

    Changes made for demo video

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 def add(a, b):
     sum = a+b  # Adding two numbers and assigning the result to a variable "sum"
+    print(sum)  # Displays the sum to the console
     return sum  # Returning the sum of the two numbers
 
 
 def subtract(a, b):
     difference = a - b
+    print(difference)  # Displays the difference to the console
     return difference
 
 
-x = add(6, 5)
-print(x)
+add(6, 5)
 
-y = subtract(6, 5)
-print(y)
+subtract(6, 5)
